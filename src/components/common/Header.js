0d4d38dc5f215308129fa15616d915932ec5c27b
MaartenGDev <maarten.grootoonk@gmail.com>
2017-05-02 20:55:38 +0200

    Added basic setup

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Todo</li>
+                    <li>Docs</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
